4baec3c4befb42a360e178f43ab3ef1a3e4868df
Rishav Bir <rishavbir305@gmail.com>
2022-11-05 16:15:55 +0530

    just start my assignment on 05-11

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import ReactDOM from "react-dom"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(
+    <>
+        <body>
+            <h1>Flight Search Engine</h1>
+            <form>
+                Name:<input type="text" size="40" /><br />
+                Origin City:<input type="text" size="40" /><br />
+                Destination City:<input type="text" size="40" /><br />  
+                Departure Date:<select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                </select>
+
+                <select>
+                    <option>Jan</option>
+                    <option>Feb</option>
+                    <option>March</option>
+                </select>
+
+                <select>
+                    <option>2001</option>
+                    <option>2002</option>
+                    <option>2003</option>
+                </select><br />
+
+               
+                Return Date:<select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                </select>
+
+                <select>
+                    <option>Jan</option>
+                    <option>Feb</option>
+                    <option>March</option>
+                </select>
+
+                <select>
+                    <option>2001</option>
+                    <option>2002</option>
+                    <option>2003</option>
+                </select><br />
+
+                Departure Time:<input type = "time"/><br/>
+                Arrival Time:<input type = "time"/><br/>
+                Price: <input type = "text"/><br/>
+                
+                <input type="Button" value="Search" />
+
+
+
+            </form>
+        </body>
+    </>,
+    document.getElementById("root")
+)
